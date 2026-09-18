cfc4df601d7938664af2481fe51eefb2f39d92ce
marimsz <marianamirelly13@gmail.com>
2026-09-18 08:15:05 -0300

    Subindo atualizações e o sonar

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,11 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
    TypeOrmModule.forRoot({
      type: "mysql",
-     host: process.env.DB_HOST,
-     port: Number(process.env.DB_PORT),
-     username: process.env.DB_USER,
-     password: process.env.DB_PASSWORD,
-     database: process.env.DB_NAME,
+     host: "localhost",
+     port: 3306,
+     username: "root",
+     password: "root",
+     database: "vendeai",
      autoLoadEntities: true,
      synchronize: false,
 }),
